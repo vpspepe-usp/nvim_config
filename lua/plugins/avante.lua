deb9e7dbb80041361ad1b0cfd526a398d0b6fa78
vpspepe-usp <victor101pepe@hotmail.com>
2026-02-06 17:59:31 +0100

    feat: avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    -- THIS IS THE CRITICAL PART:
+    build = "make",
+    opts = {
+      provider = "copilot",
+    },
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      {
+        "zbirenbaum/copilot.lua",
+        config = function()
+          require("copilot").setup {}
+        end,
+      },
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = { insert_mode = true },
+          },
+        },
+      },
+      {
+        -- Make sure setup is called for render-markdown
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "markdown", "Avante" } },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
